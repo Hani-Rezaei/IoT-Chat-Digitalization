0869a2adc52740fb5485db22789ce31d7490515f
MJ <mohammadjavad.esmaeili1994@gmail.com>
2024-11-04 12:38:22 +0100

    Initialisieren default example und Korrektur  der Makefile
    - Kopie und Korrektur der Makefile von default example als Vorlage!
    - Anpassung der Einstellungen

---
+++ default/bin/nrf52840dk/gnrc_netapi/gnrc_netapi.d
@@ -0,0 +1,145 @@
+/home/mj/Desktop/IoT-Chat-Digitalization/default/bin/nrf52840dk/gnrc_netapi/gnrc_netapi.o: \
+ /home/mj/Desktop/RIOT/sys/net/gnrc/netapi/gnrc_netapi.c \
+ /home/mj/Desktop/IoT-Chat-Digitalization/default/bin/nrf52840dk/riotbuild/riotbuild.h \
+ /home/mj/Desktop/RIOT/core/lib/include/assert.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/include/newlib/errno.h /usr/include/newlib/sys/errno.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/nano/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /home/mj/Desktop/RIOT/core/include/mbox.h \
+ /home/mj/Desktop/RIOT/core/lib/include/list.h \
+ /home/mj/Desktop/RIOT/core/lib/include/cib.h \
+ /home/mj/Desktop/RIOT/core/lib/include/assert.h \
+ /home/mj/Desktop/RIOT/core/include/msg.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ /home/mj/Desktop/RIOT/core/include/sched.h \
+ /usr/include/newlib/inttypes.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/_ansi.h \
+ /home/mj/Desktop/RIOT/core/lib/include/kernel_defines.h \
+ /home/mj/Desktop/RIOT/core/lib/include/modules.h \
+ /home/mj/Desktop/RIOT/core/lib/include/riot_version.h \
+ /home/mj/Desktop/RIOT/core/lib/include/compiler_hints.h \
+ /home/mj/Desktop/RIOT/core/lib/include/container.h \
+ /home/mj/Desktop/RIOT/core/include/native_sched.h \
+ /home/mj/Desktop/RIOT/core/lib/include/clist.h \
+ /home/mj/Desktop/RIOT/core/lib/include/list.h \
+ /home/mj/Desktop/RIOT/core/include/msg.h \
+ /home/mj/Desktop/RIOT/sys/include/net/gnrc/netreg.h \
+ /home/mj/Desktop/RIOT/core/include/sched.h \
+ /home/mj/Desktop/RIOT/sys/include/net/gnrc/nettype.h \
+ /home/mj/Desktop/RIOT/core/lib/include/modules.h \
+ /home/mj/Desktop/RIOT/sys/include/net/ethertype.h \
+ /home/mj/Desktop/RIOT/sys/include/net/protnum.h \
+ /home/mj/Desktop/RIOT/sys/include/net/gnrc/pkt.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/stdlib.h \
+ /home/mj/Desktop/RIOT/sys/include/net/gnrc/pktbuf.h \
+ /usr/include/newlib/string.h /usr/include/newlib/sys/string.h \
+ /home/mj/Desktop/RIOT/cpu/nrf52/include/cpu_conf.h \
+ /home/mj/Desktop/RIOT/cpu/cortexm_common/include/cpu_conf_common.h \
+ /home/mj/Desktop/RIOT/cpu/nrf52/include/vendor/nrf52840.h \
+ /home/mj/Desktop/RIOT/build/pkg/cmsis/CMSIS/Core/Include/core_cm4.h \
+ /home/mj/Desktop/RIOT/build/pkg/cmsis/CMSIS/Core/Include/cmsis_version.h \
+ /home/mj/Desktop/RIOT/build/pkg/cmsis/CMSIS/Core/Include/cmsis_compiler.h \
+ /home/mj/Desktop/RIOT/build/pkg/cmsis/CMSIS/Core/Include/cmsis_gcc.h \
+ /home/mj/Desktop/RIOT/build/pkg/cmsis/CMSIS/Core/Include/mpu_armv7.h \
+ /home/mj/Desktop/RIOT/cpu/nrf52/include/vendor/nrf52840_bitfields.h \
+ /home/mj/Desktop/RIOT/cpu/nrf52/include/vendor/nrf52840_peripherals.h \
+ /home/mj/Desktop/RIOT/core/include/mutex.h \
+ /home/mj/Desktop/RIOT/sys/include/architecture.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/limits.h \
+ /home/mj/Desktop/RIOT/cpu/cortexm_common/include/architecture_arch.h \
+ /home/mj/Desktop/RIOT/core/include/thread.h \
+ /home/mj/Desktop/RIOT/core/lib/include/thread_config.h \
+ /home/mj/Desktop/RIOT/core/include/thread_flags.h \
+ /home/mj/Desktop/RIOT/cpu/cortexm_common/include/thread_arch.h \
+ /home/mj/Desktop/RIOT/sys/include/net/gnrc/neterr.h \
+ /home/mj/Desktop/RIOT/core/include/thread.h \
+ /home/mj/Desktop/RIOT/sys/include/utlist.h \
+ /home/mj/Desktop/RIOT/sys/include/net/gnrc/netapi.h \
+ /home/mj/Desktop/RIOT/sys/include/net/netopt.h \
+ /home/mj/Desktop/RIOT/core/lib/include/debug.h \
+ /usr/include/newlib/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/stdio.h
+/home/mj/Desktop/IoT-Chat-Digitalization/default/bin/nrf52840dk/riotbuild/riotbuild.h:
+/home/mj/Desktop/RIOT/core/lib/include/assert.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h:
+/usr/include/newlib/errno.h:
+/usr/include/newlib/sys/errno.h:
+/usr/include/newlib/sys/reent.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/nano/newlib.h:
+/usr/include/newlib/_newlib_version.h:
+/usr/include/newlib/sys/config.h:
+/usr/include/newlib/machine/ieeefp.h:
+/usr/include/newlib/sys/features.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h:
+/usr/include/newlib/sys/cdefs.h:
+/usr/include/newlib/machine/_default_types.h:
+/usr/include/newlib/sys/_types.h:
+/usr/include/newlib/machine/_types.h:
+/usr/include/newlib/sys/lock.h:
+/home/mj/Desktop/RIOT/core/include/mbox.h:
+/home/mj/Desktop/RIOT/core/lib/include/list.h:
+/home/mj/Desktop/RIOT/core/lib/include/cib.h:
+/home/mj/Desktop/RIOT/core/lib/include/assert.h:
+/home/mj/Desktop/RIOT/core/include/msg.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h:
+/home/mj/Desktop/RIOT/core/include/sched.h:
+/usr/include/newlib/inttypes.h:
+/usr/include/newlib/sys/_intsup.h:
+/usr/include/newlib/_ansi.h:
+/home/mj/Desktop/RIOT/core/lib/include/kernel_defines.h:
+/home/mj/Desktop/RIOT/core/lib/include/modules.h:
+/home/mj/Desktop/RIOT/core/lib/include/riot_version.h:
+/home/mj/Desktop/RIOT/core/lib/include/compiler_hints.h:
+/home/mj/Desktop/RIOT/core/lib/include/container.h:
+/home/mj/Desktop/RIOT/core/include/native_sched.h:
+/home/mj/Desktop/RIOT/core/lib/include/clist.h:
+/home/mj/Desktop/RIOT/core/lib/include/list.h:
+/home/mj/Desktop/RIOT/core/include/msg.h:
+/home/mj/Desktop/RIOT/sys/include/net/gnrc/netreg.h:
+/home/mj/Desktop/RIOT/core/include/sched.h:
+/home/mj/Desktop/RIOT/sys/include/net/gnrc/nettype.h:
+/home/mj/Desktop/RIOT/core/lib/include/modules.h:
+/home/mj/Desktop/RIOT/sys/include/net/ethertype.h:
+/home/mj/Desktop/RIOT/sys/include/net/protnum.h:
+/home/mj/Desktop/RIOT/sys/include/net/gnrc/pkt.h:
+/usr/include/newlib/stdlib.h:
+/usr/include/newlib/machine/stdlib.h:
+/home/mj/Desktop/RIOT/sys/include/net/gnrc/pktbuf.h:
+/usr/include/newlib/string.h:
+/usr/include/newlib/sys/string.h:
+/home/mj/Desktop/RIOT/cpu/nrf52/include/cpu_conf.h:
+/home/mj/Desktop/RIOT/cpu/cortexm_common/include/cpu_conf_common.h:
+/home/mj/Desktop/RIOT/cpu/nrf52/include/vendor/nrf52840.h:
+/home/mj/Desktop/RIOT/build/pkg/cmsis/CMSIS/Core/Include/core_cm4.h:
+/home/mj/Desktop/RIOT/build/pkg/cmsis/CMSIS/Core/Include/cmsis_version.h:
+/home/mj/Desktop/RIOT/build/pkg/cmsis/CMSIS/Core/Include/cmsis_compiler.h:
+/home/mj/Desktop/RIOT/build/pkg/cmsis/CMSIS/Core/Include/cmsis_gcc.h:
+/home/mj/Desktop/RIOT/build/pkg/cmsis/CMSIS/Core/Include/mpu_armv7.h:
+/home/mj/Desktop/RIOT/cpu/nrf52/include/vendor/nrf52840_bitfields.h:
+/home/mj/Desktop/RIOT/cpu/nrf52/include/vendor/nrf52840_peripherals.h:
+/home/mj/Desktop/RIOT/core/include/mutex.h:
+/home/mj/Desktop/RIOT/sys/include/architecture.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/limits.h:
+/home/mj/Desktop/RIOT/cpu/cortexm_common/include/architecture_arch.h:
+/home/mj/Desktop/RIOT/core/include/thread.h:
+/home/mj/Desktop/RIOT/core/lib/include/thread_config.h:
+/home/mj/Desktop/RIOT/core/include/thread_flags.h:
+/home/mj/Desktop/RIOT/cpu/cortexm_common/include/thread_arch.h:
+/home/mj/Desktop/RIOT/sys/include/net/gnrc/neterr.h:
+/home/mj/Desktop/RIOT/core/include/thread.h:
+/home/mj/Desktop/RIOT/sys/include/utlist.h:
+/home/mj/Desktop/RIOT/sys/include/net/gnrc/netapi.h:
+/home/mj/Desktop/RIOT/sys/include/net/netopt.h:
+/home/mj/Desktop/RIOT/core/lib/include/debug.h:
+/usr/include/newlib/stdio.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h:
+/usr/include/newlib/sys/stdio.h:
---
+++ default/bin/nrf52840dk/phydat/phydat_unix.d
@@ -0,0 +1,43 @@
+/home/mj/Desktop/IoT-Chat-Digitalization/default/bin/nrf52840dk/phydat/phydat_unix.o: \
+ /home/mj/Desktop/RIOT/sys/phydat/phydat_unix.c \
+ /home/mj/Desktop/IoT-Chat-Digitalization/default/bin/nrf52840dk/riotbuild/riotbuild.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/include/newlib/math.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/nano/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h \
+ /home/mj/Desktop/RIOT/core/lib/include/assert.h \
+ /usr/include/newlib/_ansi.h /home/mj/Desktop/RIOT/sys/include/phydat.h \
+ /usr/include/newlib/sys/types.h /usr/include/newlib/sys/_stdint.h \
+ /usr/include/newlib/sys/_pthreadtypes.h \
+ /usr/include/newlib/machine/types.h \
+ /home/mj/Desktop/RIOT/core/lib/include/modules.h
+/home/mj/Desktop/IoT-Chat-Digitalization/default/bin/nrf52840dk/riotbuild/riotbuild.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h:
+/usr/include/newlib/math.h:
+/usr/include/newlib/sys/reent.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/nano/newlib.h:
+/usr/include/newlib/_newlib_version.h:
+/usr/include/newlib/sys/config.h:
+/usr/include/newlib/machine/ieeefp.h:
+/usr/include/newlib/sys/features.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h:
+/usr/include/newlib/sys/cdefs.h:
+/usr/include/newlib/machine/_default_types.h:
+/usr/include/newlib/sys/_types.h:
+/usr/include/newlib/machine/_types.h:
+/usr/include/newlib/sys/lock.h:
+/home/mj/Desktop/RIOT/core/lib/include/assert.h:
+/usr/include/newlib/_ansi.h:
+/home/mj/Desktop/RIOT/sys/include/phydat.h:
+/usr/include/newlib/sys/types.h:
+/usr/include/newlib/sys/_stdint.h:
+/usr/include/newlib/sys/_pthreadtypes.h:
+/usr/include/newlib/machine/types.h:
+/home/mj/Desktop/RIOT/core/lib/include/modules.h:
