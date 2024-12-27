835c01667cd45fb07aaa5acf70c6172dd6d873d7
Mohammadjavad <mohammadjavad.esmaeili1994@gmail.com>
2024-12-27 17:52:26 +0100

    Repo clean

--- gnrc_border_router/bin/nrf52840dongle/random/musl_lcg.d
+++
@@ -1,6 +0,0 @@
-/home/mj/Desktop/IoT-Chat-Digitalization/gnrc_border_router/bin/nrf52840dongle/random/musl_lcg.o: \
- /home/mj/Desktop/RIOT/sys/random/musl_lcg.c \
- /home/mj/Desktop/IoT-Chat-Digitalization/gnrc_border_router/bin/nrf52840dongle/riotbuild/riotbuild.h \
- /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h
-/home/mj/Desktop/IoT-Chat-Digitalization/gnrc_border_router/bin/nrf52840dongle/riotbuild/riotbuild.h:
-/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h:
