7751f8d6ff2185e562def08b513dff333bcb815a
Mohammadjavad <mohammadjavad.esmaeili1994@gmail.com>
2025-01-06 15:38:47 +0100

    saul und mqtt kombination

---
+++ saul/bin/nrf52840dk/paho-mqtt-packet/MQTTSubscribeClient.d
@@ -0,0 +1,53 @@
+/home/mj/Desktop/IoT-Chat-Digitalization/saul/bin/nrf52840dk/paho-mqtt-packet/MQTTSubscribeClient.o: \
+ /home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTSubscribeClient.c \
+ /home/mj/Desktop/IoT-Chat-Digitalization/saul/bin/nrf52840dk/riotbuild/riotbuild.h \
+ /home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTPacket.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTConnect.h \
+ /home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTPublish.h \
+ /home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTSubscribe.h \
+ /home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTUnsubscribe.h \
+ /home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTFormat.h \
+ /home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/StackTrace.h \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/nano/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h \
+ /home/mj/Desktop/RIOT/core/lib/include/assert.h \
+ /usr/include/newlib/sys/stdio.h /usr/include/newlib/string.h \
+ /usr/include/newlib/sys/string.h
+/home/mj/Desktop/IoT-Chat-Digitalization/saul/bin/nrf52840dk/riotbuild/riotbuild.h:
+/home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTPacket.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h:
+/home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTConnect.h:
+/home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTPublish.h:
+/home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTSubscribe.h:
+/home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTUnsubscribe.h:
+/home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/MQTTFormat.h:
+/home/mj/Desktop/RIOT/build/pkg/paho-mqtt/MQTTPacket/src/StackTrace.h:
+/usr/include/newlib/stdio.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/nano/newlib.h:
+/usr/include/newlib/_newlib_version.h:
+/usr/include/newlib/sys/config.h:
+/usr/include/newlib/machine/ieeefp.h:
+/usr/include/newlib/sys/features.h:
+/usr/include/newlib/sys/cdefs.h:
+/usr/include/newlib/machine/_default_types.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h:
+/usr/include/newlib/sys/reent.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/sys/_types.h:
+/usr/include/newlib/machine/_types.h:
+/usr/include/newlib/sys/lock.h:
+/home/mj/Desktop/RIOT/core/lib/include/assert.h:
+/usr/include/newlib/sys/stdio.h:
+/usr/include/newlib/string.h:
+/usr/include/newlib/sys/string.h:
