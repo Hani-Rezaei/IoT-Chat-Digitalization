7dc1135dde1133a11c77c38459f94092c1f1b420
me-infotech <mj.esmaeili73@gmail.com>
2025-01-26 21:25:27 +0100

    Änderungen nach dem Mergen

---
+++ Nimble/nimble_gatt/bin/nrf52840dk/nimble_host/ble_sm_alg.d
@@ -0,0 +1,43 @@
+/home/mj/Desktop/IoT-Chat-Digitalization/nimble_gatt/bin/nrf52840dk/nimble_host/ble_sm_alg.o: \
+ /home/mj/Desktop/RIOT/build/pkg/nimble/nimble/host/src/ble_sm_alg.c \
+ /home/mj/Desktop/IoT-Chat-Digitalization/nimble_gatt/bin/nrf52840dk/riotbuild/riotbuild.h \
+ /usr/include/newlib/inttypes.h /usr/include/newlib/nano/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
+ /usr/include/newlib/sys/_intsup.h /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/include/newlib/string.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h \
+ /home/mj/Desktop/RIOT/core/lib/include/assert.h \
+ /usr/include/newlib/sys/string.h \
+ /home/mj/Desktop/RIOT/build/pkg/nimble/porting/npl/riot/include/syscfg/syscfg.h \
+ /home/mj/Desktop/RIOT/build/pkg/nimble/nimble/include/nimble/nimble_opt.h \
+ /home/mj/Desktop/RIOT/build/pkg/nimble/nimble/include/nimble/nimble_opt_auto.h
+/home/mj/Desktop/IoT-Chat-Digitalization/nimble_gatt/bin/nrf52840dk/riotbuild/riotbuild.h:
+/usr/include/newlib/inttypes.h:
+/usr/include/newlib/nano/newlib.h:
+/usr/include/newlib/_newlib_version.h:
+/usr/include/newlib/sys/config.h:
+/usr/include/newlib/machine/ieeefp.h:
+/usr/include/newlib/sys/features.h:
+/usr/include/newlib/sys/_intsup.h:
+/usr/include/newlib/_ansi.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h:
+/usr/include/newlib/string.h:
+/usr/include/newlib/sys/reent.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/sys/cdefs.h:
+/usr/include/newlib/machine/_default_types.h:
+/usr/include/newlib/sys/_types.h:
+/usr/include/newlib/machine/_types.h:
+/usr/include/newlib/sys/lock.h:
+/home/mj/Desktop/RIOT/core/lib/include/assert.h:
+/usr/include/newlib/sys/string.h:
+/home/mj/Desktop/RIOT/build/pkg/nimble/porting/npl/riot/include/syscfg/syscfg.h:
+/home/mj/Desktop/RIOT/build/pkg/nimble/nimble/include/nimble/nimble_opt.h:
+/home/mj/Desktop/RIOT/build/pkg/nimble/nimble/include/nimble/nimble_opt_auto.h:
